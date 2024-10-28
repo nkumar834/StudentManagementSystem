938daab2c999291f1809d597522419dfafe936f5
Niraj Thakur <nkumar834@rku.ac.in>
2024-10-28 11:43:06 +0530

    Add files via upload

---
+++ Attendance.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class Attendance : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\DELL\source\repos\StudentManagementSystem\StudentManagementSystem\school_db.mdf;Integrated Security=True";
+
+        private Attendanceinfo _attendanceinfo;
+
+        public Attendance(Attendanceinfo attendanceinfo)
+        {
+            InitializeComponent();
+            _attendanceinfo = attendanceinfo;
+        }
+
+        void connection()
+        {
+            con = new SqlConnection(connectionString);
+            con.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            _attendanceinfo.Show();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string program = comboBoxprogram.SelectedItem.ToString();
+            string semester = comboBoxsemester.SelectedItem.ToString();
+            string division = comboBoxdivision.SelectedItem.ToString();
+
+            string attendanceStatus = string.Join(",", checkedListBoxrollno.CheckedItems.Cast<string>());
+            byte[] attendanceBytes = Encoding.UTF8.GetBytes(attendanceStatus);
+            string query = "INSERT INTO attendance (date, program, semester, division, attendance) VALUES ('"
+                           + dateTimePickerdate.Value.ToString("yyyy-MM-dd") + "', '"
+                           + program + "', '"
+                           + semester + "', '"
+                           + division + "', CONVERT(varbinary, '"
+                           + Convert.ToBase64String(attendanceBytes) + "'))"; 
+
+            connection(); 
+            cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery(); 
+            MessageBox.Show("Data Inserted successfully");
+            con.Close(); 
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            
+            dateTimePickerdate.Value = DateTime.Now;
+            comboBoxprogram.SelectedIndex = -1;
+            comboBoxsemester.SelectedIndex = -1;
+            comboBoxdivision.SelectedIndex = -1;
+
+            for (int i = 0; i < checkedListBoxrollno.Items.Count; i++)
+            {
+                checkedListBoxrollno.SetItemChecked(i, false);
+            }
+        }
+
+        private void Attendance_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            _attendanceinfo.Show();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+            _attendanceinfo.Show();
+        }
+
+        private void Attendance_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Attendanceinfo.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class Attendanceinfo : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds;
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\DELL\source\repos\StudentManagementSystem\StudentManagementSystem\school_db.mdf;Integrated Security=True";
+
+        public Attendanceinfo()
+        {
+            InitializeComponent();
+        }
+
+        void connection()
+        {
+            con = new SqlConnection(connectionString);
+            con.Open();
+        }
+
+        void fillGrid(string searchTerm = "")
+        {
+            connection();
+
+            string query;
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                query = "SELECT * FROM attendance WHERE program LIKE '%" + searchTerm + "%'";
+            }
+            else
+            {
+                query = "SELECT * FROM attendance";
+            }
+
+            da = new SqlDataAdapter(query, con);
+            ds = new DataSet();
+            da.Fill(ds);
+            dataGridView3.DataSource = ds.Tables[0];
+            con.Close();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Attendance at = new Attendance(this);
+            at.ShowDialog();
+        }
+
+        private void Attendanceinfo_Load(object sender, EventArgs e)
+        {
+            fillGrid(); 
+        }
+
+       
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+            fillGrid(textBox6.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Main back = new Main();
+            back.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            fillGrid();
+        }
+
+        private void Attendanceinfo_Load_1(object sender, EventArgs e)
+        {
+            fillGrid();
+        }
+    }
+}
